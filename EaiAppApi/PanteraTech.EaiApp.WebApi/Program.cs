0baf9f8ddf1561d0c8ddce3adbaa2ba1ea9c1920
Julio_Nascimento2 <julio.nascimento@cwi.com.br>
2021-12-20 13:51:12 -0300

    Subindo Estrutura base do back

---
+++ EaiAppApi/PanteraTech.EaiApp.WebApi/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+namespace PanteraTech.EaiApp.WebApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            var config = new ConfigurationBuilder().AddCommandLine(args).Build();
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseConfiguration(config)
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
